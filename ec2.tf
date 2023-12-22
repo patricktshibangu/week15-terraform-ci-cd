d978a1a46c341cd2cf1cbded66f55162235d23ec
Patrick <patricktshibangu@gmail.com>
2023-12-21 21:52:18 -0500

    new change

--- ec2.tf
+++ ec2.tf
@@ -5,3 +5,11 @@ resource "aws_instance" "server1" {
     Name = "Terraform-server"
   }
 }
+
+resource "aws_instance" "server2" {
+  instance_type = var.instance_type
+  ami = data.aws_ami.ami1.id
+  tags = {
+    Name = "Terraform-server2"
+  }
+}
